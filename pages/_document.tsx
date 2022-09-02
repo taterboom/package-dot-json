77a8c66c996c04c1c7cc0f43a3a57803aca5b4fe
xueyong <1085523029@qq.com>
2022-09-02 16:51:15 +0800

    :sparkles: add GA

---
+++ pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from "next/document"
+import { isExtension } from "../utils/env"
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {!isExtension && (
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-P6WQPC2');`,
+              }}
+            ></script>
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
