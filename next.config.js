5fb308c39cfee2212af023d832ce6fb924092e40
xueyong <1085523029@qq.com>
2022-09-04 12:44:28 +0800

    :tada: doc

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -8,6 +18,7 @@ const nextConfig = {
       unoptimized: true,
     },
   },
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
